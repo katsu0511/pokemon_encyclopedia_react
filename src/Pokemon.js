d02eabb5637d8a108a3c511c9637c8384c8ed8c9
Katsuya <haradakatsuya190511@gmail.com>
2025-02-10 16:48:36 -0800

    make div element to show information

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -8,8 +8,35 @@ const fetchPokemon = async () => {
 
 export default function Pokemon() {
   const { data } = useQuery('pokemon', fetchPokemon);
+  const abilities = [];
+  const types = [];
+
+  data.abilities.forEach((ability) => {
+    abilities.push(
+      <li>{ability.ability.name}</li>
+    )
+  });
+  data.types.forEach((type) => {
+    types.push(
+      <li>{type.type.name}</li>
+    )
+  });
 
   return (
-    <section className='pokemon'>{data.name}</section>
+    <section className='pokemon'>
+      <div>
+        <p>No. {data.id}</p>
+        <p>{data.name}</p>
+        <p>Height: {data.height}</p>
+        <p>Weight: {data.weight}</p>
+        <div>Abilities:
+          <ul>{abilities}</ul>
+        </div>
+        <div>Type:
+        <ul>{types}</ul>
+        </div>
+        <img src={data.sprites.versions["generation-iii"].emerald.front_default} alt={data.name} />
+      </div>
+    </section>
   );
 }
